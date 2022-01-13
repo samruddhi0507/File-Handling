cc2351e17db2d5bbae5f10184d0668f0b3975452
samruddhi randive <samruddhirandive97@gmail.com>
2022-01-13 20:30:39 +0530

    my project

---
+++ src/main/FileHandler.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class FileHandler {
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		String fileName = "text.txt";
+		//helps find path to text files, needed for appending method
+		Path path = FileSystems.getDefault().getPath(fileName).toAbsolutePath();
+		System.out.println(path);
+		
+		
+		
+		FileHandlingMethods fileHandle = new FileHandlingMethods();
+		fileHandle.DeleteFileIfExists("text.txt");
+		System.out.println("Fresh start");
+		System.out.println();
+		
+		File myFile = fileHandle.createFile("text.txt");
+		
+		fileHandle.writeFile("Hello World!");
+		
+		fileHandle.readFile("text.txt");
+		
+		System.out.println("End of test 1");
+		System.out.println();
+		
+		fileHandle.appendToFile("\nHello World to you too!", path.toString());
+		
+		fileHandle.readFile("text.txt");
+		
+		System.out.println("End of test 2");
+		System.out.println();
+		
+		fileHandle.appendToFile("End of test. Have a nice day.", path.toString());
+		
+		fileHandle.readFile("text.txt");
+		
+		System.out.println("End of test 3");
+	}
+	
+	
+}
